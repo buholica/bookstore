2e8f58b60750211ec705aa788dae1b4a6e897daf
Oksana <oxanachyrkova9@gmail.com>
2024-05-25 19:24:54 +0300

    Add faq and cart pages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase
 from sqlalchemy import Integer, String, Text, ForeignKey, Column
@@ -48,20 +48,27 @@ def homepage():
     return render_template("index.html", books=new_books, year=CURRENT_YEAR)
 
 
+@app.route("/faq")
+def show_faq():
+    return render_template("faq.html")
+
+
 @app.route("/fiction")
 def show_fiction():
     result = db.session.execute(db.select(Books))
     all_books = result.scalars().all()
+    title = "Fiction"
     books = [book for book in all_books if book.type == "Fiction"]
-    return render_template("listing.html", books=books)
+    return render_template("listing.html", books=books, title=title)
 
 
 @app.route("/non-fiction")
 def show_nonfiction():
     result = db.session.execute(db.select(Books))
     all_books = result.scalars().all()
+    title = "Non-fiction"
     books = [book for book in all_books if book.type == "Nonfiction"]
-    return render_template("listing.html", books=books)
+    return render_template("listing.html", books=books, title=title)
 
 
 @app.route("/log_in")
@@ -72,6 +79,27 @@ def log_in():
 @app.route("/sign_up")
 def sign_up():
     return render_template("registration.html")
+
+
+@app.route("/cart")
+def show_cart():
+    cart_items = session.get("cart", [])
+    return render_template("cart.html", cart_items=cart_items)
+
+
+@app.route("/add-to-cart", methods=['POST'])
+def add_to_cart():
+    item_id = request.form.get('item_id')
+    book_id = Books.query.filter_by(id=item_id).first()
+    print(item_id)
+    print(book_id)
+
+    if item_id == book_id:
+        if "cart" not in session:
+            session["cart"] = []
+        session["cart"].append(item_id)
+    return redirect(url_for("show_cart"))
+
 
 
 if __name__ == "__main__":
